08a80a21104550a9d6c15f4106ac97345c6e5686
John Doe <johndoe@email.com>
2024-05-12 15:13:14 +0100

    push_commit

--- SQL_project/3_most_in_demand_skills.sql
+++ SQL_project/3_most_in_demand_skills.sql
@@ -22,6 +22,7 @@ INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
 WHERE
     job_title_short = 'Data Scientist' 
     AND job_work_from_home = True 
+    AND salary_year_avg IS NOT NULL
 GROUP BY
     skills
 ORDER BY
